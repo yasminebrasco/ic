1af8f6e3b42b5ed66914821e847f0704fb9293ee
yasminebrasco <yasminebrasco@gmail.com>
2016-12-17 23:35:16 -0200

    Criação do grid

--- redes neurais.R
+++ redes neurais.R
@@ -57,6 +57,30 @@ data=as.h2o(data)
 
 table(dados[[1]])
 
+hidden_opt = lapply(1:100, function(x)10+sample(50,
+  sample(10), replace=TRUE))
+
+l1_opt = seq(1e-6,1e-3,1e-6)
+hyper_params = list(hidden = hidden_opt, l1 = l1_opt)
+
+search_criteria = list(strategy = "RandomDiscrete",
+  max_models = 10, max_runtime_secs = 100,
+  seed=123456)
+
+model_grid <- h2o.grid("deeplearning",
+  grid_id = "mygrid",
+  hyper_params = hyper_params, 
+  search_criteria = search_criteria,
+  x = names(data)[-1],
+  y = "amostras",
+  distribution = "multinomial",
+  training_frame = data,
+  #validation_frame = test,
+  score_interval = 2,
+  epochs = 1000,
+  stopping_rounds = 3,
+  stopping_tolerance = 0.05,
+  stopping_metric = "misclassification")
+
 fit=h2o.deeplearning(x=names(data)[-1], y="amostras", training_frame = data, nfolds = 5
-  ,hidden = c(32,16)
-)
+  ,hidden = model_grid)
